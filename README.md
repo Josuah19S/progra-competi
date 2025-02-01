ee2f58881107d3cd034355cc90a8084145aed643
Josuah19S <abraham.carbajal2@unmsm.edu.pe>
2025-02-01 02:02:42 -0500

    Repo organization

---
+++ README.md
@@ -0,0 +1,2 @@
+# Progra Competi
+UNMSM competitive programmer
---
+++ template.cpp
@@ -0,0 +1,21 @@
+/*
+Date: //2025
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+
+    while(t--){
+
+    }
+    
+    return 0;
+}
